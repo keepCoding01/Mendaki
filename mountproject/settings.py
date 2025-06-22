42189a9ae63c171e9c7a78c23a447710a01a71d4
tasya <tasyasyafriza@gmail.com>
2025-06-22 08:36:34 +0700

    set

--- mountproject/settings.py
+++ mountproject/settings.py
@@ -179,3 +179,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+
+urlpatterns = [
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
